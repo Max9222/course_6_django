061668f41ac8d10174afcb8dd631925261784556
Max9222 <maxoog@bk.ru>
2023-10-15 21:49:04 +0300

    Создал сообщение для рассылки. Следующий шаг подключение почтового сервиса.

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from  newsletter.models import Newsletter
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('client', 'email', )
+    list_filter = ('client',)
+
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+from django.urls import reverse
 
-# Create your views here.
+from main.models import Client
+from newsletter.models import Newsletter
+class NewsletterCreateView(CreateView):
+    model = Newsletter
+    fields = ('client', 'name', 'email', 'comment',)
+
+    def get_success_url(self):
+        return reverse('main:client_view', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['client'] = get_object_or_404(Client, pk=self.kwargs.get('pk'))
+        return context_data
+
